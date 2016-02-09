a60cef4413e2b34c449a1f67d5440eac441d7ac7
rlaverack <rlaverack@gmail.com>
2016-02-09 11:02:33 -0500

    Added first iteration of capstone model

---
+++ Capstone/Code_and_Data/SVM.py
@@ -0,0 +1,31 @@
+
+import sys
+from time import time
+from collections import Counter
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#Load training and testing data
+features_train = pd.read_csv('X.csv')
+features_test = pd.read_csv('X_test.csv')
+labels_train = pd.read_csv('y.csv').pop('SLIDE')
+labels_test = pd.read_csv('y_test.csv').pop('SLIDE')
+
+#Define Model
+clf = svm.SVC(kernel="rbf",C=1000)
+
+#Fit Data to Model
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("Training time:", round(time() - t0, 3), "s")
+
+#Predict on test set
+t1 = time()
+pred = clf.predict(features_test)
+print ("predicting time:", round(time() - t1, 3), "s")
+
+#Compare prediction to test labels
+from sklearn.metrics import accuracy_score
+print(accuracy_score(pred, labels_test))
--- Machine_Learning/svm/svm_author_id.py
+++ Machine_Learning/svm/svm_author_id.py
@@ -12,19 +12,35 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
-
+from collections import Counter
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+clf = svm.SVC(kernel="rbf",C=10000)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("Training time:", round(time() - t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print ("predicting time:", round(time() - t1, 3), "s")
+
+c = Counter( pred )
+print( c.items() )
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(pred, labels_test))
 #########################################################
